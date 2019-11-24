ca680b66a4c8388a52b17bc09670b741c1a0b5d9
dmitrynjg <49000073+dmitrynjg@users.noreply.github.com>
2019-11-24 16:41:16 +0300

    loading lw5
    first loading lw5

---
+++ lw5/task1/script.js
@@ -0,0 +1,13 @@
+
+  function replaceString(text, searchStr, newStr) {
+    if (text.indexOf(searchStr) !== -1) {
+      text = text.replace(RegExp(searchStr, 'g'), newStr);
+      return text;
+    }
+    else {
+      return "Ничего не найдено";
+    }
+  }
+
+
+
