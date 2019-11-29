4b827284e39b909ffdfd762e12516dcaaa521a63
dmitrynjg <49000073+dmitrynjg@users.noreply.github.com>
2019-11-29 16:15:37 +0300

    added task3 and npm

---
+++ lw6/src/task2 chat/script.js
@@ -0,0 +1,36 @@
+var btnSend = document.querySelector('.chat-form-wrap-send__btn');
+function addMessage(iSender) {
+  'use strict';
+  var messageText = document.querySelector('.chat-form-wrap__input').value;
+  if (messageText.length > 0) {
+    messageText = messageText.replace(/</g, '&lt;');
+    messageText = messageText.replace(/>/g, '&gt;');
+    var messageOutput = document.createElement('div');
+    var name,avatar;
+    if(iSender === true){
+      messageOutput.className = 'chat-messages-bubble chat-messages-bubble_right bubble-message';
+      name = 'Dmitry Shashkov';
+      avatar = 'images/dmitry.jpg';
+    }
+    else{
+      messageOutput.className = 'chat-messages-bubble chat-messages-bubble_left bubble-message';
+      name = 'Alexander Kalinin';
+      avatar = 'images/alexander.jpg';
+    }
+    var messageTemplate = '<div class=\'bubble-message-user\'>';
+    messageTemplate += '<div class=\'bubble-message-user-avatar\'>';
+    messageTemplate += '<img src=\''+avatar+'\' alt=\'avatar user\' class=\'bubble-message-user-avatar__photo\' alt=\'аватарка\'>';
+    messageTemplate += '</div>';
+    messageTemplate += '<div class=\'bubble-message-user__name\'>'+name+'</div>';
+    messageTemplate += '</div>';
+    messageTemplate += '<div class=\'bubble-message__text\'>'+messageText+'</div>';
+    messageOutput.innerHTML = messageTemplate;
+    document.querySelector('.chat-messages-output').appendChild(messageOutput);
+    var chat = document.querySelector('.chat-messages');
+    chat.scrollTop = chat.scrollHeight;
+  }
+}
+btnSend.onclick = function(){
+  'use strict';
+  addMessage(true);
+};
---
+++ lw6/src/task3 reg/script.js
@@ -0,0 +1,18 @@
+window.onload = function(){
+var btnReg = document.querySelector('.form__btn');
+function dataCheck(){
+var fioUser = document.querySelector('.form-reg__fio');
+var emailUser = document.querySelector('.form-reg__email');
+var passwordUser = document.querySelectorAll('.form-reg__password');
+if((fioUser.value.lenght = 0) || (fioUser.value.search(/[А-ЯA-Z][a-zа-я]{0,} [А-ЯA-Z][a-zа-я]{0,} [А-ЯA-Z][a-zа-я]{0,}/g) != 0) ){
+  fioUser.classList.add('form-reg__input_error');
+  fioUser.parentNode.classList.add('form-reg_fioerror');
+}
+else{
+    fioUser.classList.remove('form-reg__input_error');
+  fioUser.parentNode.classList.remove('form-reg_fioerror');
+}
+
+}
+btnReg.onclick = dataCheck;
+}
